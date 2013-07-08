bb619021768ce1c86ba17b8a6633729a958b3482
Jonathan Lai <jonathanlai@eltux.net>
2013-07-07 21:34:23 -0700

    change to config.rb

--- config.rb
+++ config.rb
@@ -72,7 +72,7 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 activate :syntax
 
-set :css_dir, 'stylesheets'
+set :css_dir, 'stylesheets/'
 
 set :js_dir, 'javascripts'
 
@@ -86,19 +86,19 @@ page "/feed.xml", :layout => false
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
   # Or use a different image path
-  set :http_path, "/images/"
+  # set :http_path, "/images/"
 end
 
 activate :deploy do |deploy|
